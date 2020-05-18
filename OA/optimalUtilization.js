1afd6b25afe4976a2d22345aeb537e0970368c24
laffed <roarklaffed@gmail.com>
2020-05-17 19:14:46 -0500

    .

--- OA/optimalUtilization.js
+++ OA/optimalUtilization.js
@@ -9,27 +9,49 @@ target = 7
 Output: [[2, 1]]
 */
 
-//DISLAIMER: function only return the most optimal value
+//credit: popeye_the_sailort
 
-function optimalUtilization(a, b, target) {
-    let potential = [];
-    for (let i = 0; i < a.length; i++) {
-        for (let j =0; j < b.length; j++) {
-            let sum = a[i][1] + b[j][1];
-            let aId = a[i][0];
-            let bId = b[j][0];
-            if (sum <= target) {
-                potential.push([aId, bId, sum]);
+function optimalUtilization(a, b, tar) {
+    //sort both inputs
+    a.sort((a,b) => a[1] - b[1]);
+    b.sort((a,b) => a[1] - b[1]);
+
+    const result = [];
+    let max = Number.NEGATIVE_INFINITY;
+    const m = a.length;
+    const n = b.length;
+
+    let i = 0;
+    let j = n - 1;
+
+    while (i < m && j >= 0) {
+        let sum = a[i][1] + b[j][1];
+        if (sum > tar) {
+            j--;
+        } else {
+            if (max <= sum) {
+                if (max < sum) {
+                    //update max and delete prev result
+                    max = sum;
+                    result.length = 0;
+                }
+                result.push([a[i][0], b[j][0]]);
+                //check next lower j if the sum is the same
+                //if so, these ids need to be included in the result
+                let index = j - 1
+                while (index >= 0 && b[index][1] === b[index+1][1]) {
+                    result.push([a[i][0], b[index][0]]);
+                    index -= 1;
+                }
             }
+            i++;
         }
     }
-    potential.sort((a,b) => b[2] - a[2]);
-    const result = potential[0].slice(0,2);
     return result;
 }
-
 let a = [[1, 2], [2, 4], [3, 6]];
 let b = [[1, 2]];
-
+let a2 = [[1, 3], [2, 5], [3, 7], [4, 10]];
+let b2 = [[1, 2], [2, 3], [3, 4], [4, 5]];
 console.log(optimalUtilization(a, b, 7));
-
+console.log(optimalUtilization(a2,b2, 10));
