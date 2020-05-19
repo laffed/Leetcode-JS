a45bc9510d86571cae6338ec04e707c2ae81ee30
laffed <roarklaffed@gmail.com>
2020-05-19 09:34:14 -0500

    .

---
+++ moveZerosToEnd.js
@@ -0,0 +1,26 @@
+/*
+Move Zeros To End
+Given a static-sized array of integers arr, move all zeroes in the array to the end of the array. You should preserve the relative order of items in the array.
+
+We should implement a solution that is more efficient than a naive brute force.
+
+Examples:
+
+input:  arr = [1, 10, 0, 2, 8, 3, 0, 0, 6, 4, 0, 5, 7, 0]
+output: [1, 10, 2, 8, 3, 6, 4, 5, 7, 0, 0, 0, 0, 0]
+*/
+
+function moveZeros(arr) {
+    let idx = 0;
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] !== 0) {
+            arr[idx] = arr[i];
+            arr[i] = idx === i ? arr[i] : 0;
+            idx += 1;
+        }
+    }
+    return arr;
+}
+
+const input = [1, 10, 0, 2, 8, 3, 0, 0, 6, 4, 0, 5, 7, 0];
+console.log(moveZeros(input));
